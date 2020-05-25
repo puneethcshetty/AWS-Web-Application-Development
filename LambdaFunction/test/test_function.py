affa57000bada5f848de4d3eceecf338496c041d
harshaJha18 <47431815+harshaJha18@users.noreply.github.com>
2020-05-25 10:20:51 -0400

    Update test_function.py

--- LambdaFunction/test/test_function.py
+++ LambdaFunction/test/test_function.py
@@ -4,11 +4,9 @@ import numpy as np
 from src.lambda_function import *
 from test import *
 
-
 def test(event, context):
     s3 = boto3.client("s3")
-    json_file = s3.get_object(Bucket = "mainlamdbas3", Key = "test.json")
-    resp = my_json_data(json_file)
-    print(resp)
-    assert resp=={'statusCode': 200, 'body': '[3273, 3270, 3270, 3518, 3519]'}
-    return  "Test is successfully run!!"
+    json_file = s3.get_object(Bucket = "mainlamdbas3", Key = "test.json") # Getting object from bucket name and json key name
+    resp = my_json_data(json_file) # Calling my_json_data function from the lambda_function.py file
+    assert resp=={'statusCode': 200, 'body': '[3273, 3270, 3270, 3518, 3519]'} #  testing if a condition in our code returns True, if not, the program will raise an AssertionError. 
+    return  "Test is successfully run!!" # if it return True. It will print the statement.
