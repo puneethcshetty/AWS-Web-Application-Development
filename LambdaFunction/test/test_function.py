f3e94ad475c378282e4dcabd5ebb2dab1ba2cb7b
harshaJha18 <47431815+harshaJha18@users.noreply.github.com>
2020-05-25 10:13:05 -0400

    Add files via upload

---
+++ LambdaFunction/test/test_function.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+import numpy as np
+from src.lambda_function import *
+from test import *
+
+
+def test(event, context):
+    s3 = boto3.client("s3")
+    json_file = s3.get_object(Bucket = "mainlamdbas3", Key = "test.json")
+    resp = my_json_data(json_file)
+    print(resp)
+    assert resp=={'statusCode': 200, 'body': '[3273, 3270, 3270, 3518, 3519]'}
+    return  True
